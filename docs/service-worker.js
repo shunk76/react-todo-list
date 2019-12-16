3e15d4e6bed3ebfe77c55630d527354ed39569a1
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2019-12-16 19:19:10 +0900

    🎉 priority ボタンの :focus の outline を変更

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d89520ecf29bf2571aff1cab1943890c.js"
+  "./precache-manifest.48d2a504cad6079e68c36df94bfcab68.js"
 );
 
 self.addEventListener('message', (event) => {
